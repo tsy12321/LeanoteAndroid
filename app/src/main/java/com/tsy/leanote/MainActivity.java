77627cb29e29d72c279e4a0193ca5ae4e3a751c6
tangsiyuan <tangsiyuan@corp-ci.com>
2016-12-13 11:18:59 +0800

    first commit

---
+++ app/src/main/java/com/tsy/leanote/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tsy.leanote;
+
+import android.os.Bundle;
+
+import com.tsy.leanote.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
